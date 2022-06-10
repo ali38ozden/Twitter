7e7b5c554654bf093e0b606b66342f9e1382d8c9
ali38ozden <susaali5@gmail.com>
2022-06-10 09:06:59 +0300

    Update Twitter.py

--- Twitter.py
+++ Twitter.py
@@ -5,41 +5,47 @@ from time import sleep
 kacKereGecildi=0
 kacKisiTakipEdildi=0
 
-kullaniciAdi=input("KULLANICI ADI: ")
-kullaniciSifre=input("KULLANICI SİFRE: ")
+Giris_Yapildimi=True
 
-driver=webdriver.Chrome(executable_path="chromedriver.exe")
+#-----------------bizim belirlediğimiz
+Bir_Sayfada_KacKisi_Takip=0
+
+options = webdriver.ChromeOptions()
+options.add_argument('--user-data-dir=C:\\Users\\ali\\AppData\\Local\\Google\\Chrome\\User Data')
+driver = webdriver.Chrome(chrome_options=options)
 driver.maximize_window()
-driver.get("https://twitter.com/i/flow/login")
+driver.get("https://twitter.com")
 sleep(4)
 
-driver.find_element_by_xpath("/html/body/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]/label/div/div[2]/div/input")\
-    .send_keys(kullaniciAdi)
-sleep(0.5)
 
-driver.find_element_by_xpath("/html/body/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[6]/div")\
-    .click()
+if Giris_Yapildimi==False:
+    kullaniciAdi=input("KULLANICI ADI: ")
+    kullaniciSifre=input("KULLANICI SİFRE: ")
+    driver.find_element_by_xpath("/html/body/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]/label/div/div[2]/div/input")\
+        .send_keys(kullaniciAdi)
+    sleep(0.5)
 
-sleep(1.5)
-
-try:
-    driver.find_element_by_xpath("/html/body/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input")\
-        .send_keys(kullaniciSifre)
-except:
-    print("\n\n\n\nkullanici adi hatalı")
-    driver.close()
-    exit("Tekrar deneyeiniz")
-sleep(0.5)
-driver.find_element_by_xpath("/html/body/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div/div")\
-   .click()
+    driver.find_element_by_xpath("/html/body/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[6]/div")\
+        .click()
+    sleep(1.5)
+    try:
+        driver.find_element_by_xpath("/html/body/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input")\
+            .send_keys(kullaniciSifre)
+    except:
+        print("\n\n\n\nkullanici adi hatalı")
+        driver.close()
+        exit("Tekrar deneyeiniz")
+    sleep(0.5)
+    driver.find_element_by_xpath("/html/body/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div/div")\
+        .click()
 
 sleep(5)
-driver.get("https://twitter.com/i/connect_people?user_id=1529431017177612290")
+driver.get("https://twitter.com/i/connect_people")
 sleep(5)
 
 for i in range(30):
 
-    for a in range(15):
+    for a in range(Bir_Sayfada_KacKisi_Takip):
 
         b=str(a+3)
         try: 
